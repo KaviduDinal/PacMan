008ae5f9ab08c346fa57df1a8d9d303f87149a75
KaviduDinal <dinalkavidu5@gmail.com>
2025-11-21 23:42:31 +0530

    Create the first game board with java

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int rowcount=21;
+        int coloumncount=19;
+        int tileSize=32;
+
+        int boardWidth=coloumncount*tileSize;
+        int boardHeight=rowcount*tileSize;
+
+        JFrame frame =new JFrame("PacMan");
+        frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+    }
+}
